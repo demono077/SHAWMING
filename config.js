7fdf6b4ee5937b35e67910da8c3a0e482c9fb136
demono077 <ahmedeopop2@gmail.com>
2026-05-21 05:17:03 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    // توكن البوت الخاص بك
+    token: "8962956921:AAG9MQSwUZuNl7pPbIRToaRyQhR68_i3PJE",
+    
+    // أيدي المطور (أيدي حسابك)
+    adminId: 7715555599,
+    
+    // يوزر جروب الإضافة (بدون علامة @)
+    // ملاحظة: تأكد أن هذا يوزر لجروب حقيقي وليس بوت
+    addGroupUsername: "IMAFIAl1_bot", 
+    
+    // رابط الجروب السري
+    secretGroupLink: "[https://t.me/+Au-wTZaUQdU4MGFk](https://t.me/+Au-wTZaUQdU4MGFk)",
+    
+    // عدد الأعضاء المطلوب إضافتهم
+    targetMembers: 50
+};
